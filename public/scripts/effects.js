9904a50d30f07a098f266653362e69a6839432de
jazheng <59896127+jazheng1@users.noreply.github.com>
2023-05-25 19:55:34 -0400

    done for now

--- public/scripts/effects.js
+++ public/scripts/effects.js
@@ -142,3 +142,26 @@ function unfade(element) {
         op += op * 0.1;
     }, 10);
 }
+
+//page load effect
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("visible");
+          } else {
+            reveals[i].classList.remove("visible");
+          }
+      }
+
+      
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal()
